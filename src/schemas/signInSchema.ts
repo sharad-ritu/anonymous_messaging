155acacf0a8cd881c0a6e57a4788fee5cbac3022
Sharad <sharadritu.58.69@gmail.com>
2024-05-27 22:49:40 +0545

    initial db setup

--- src/schemas/signInSchema.ts
+++ src/schemas/signInSchema.ts
@@ -2,5 +2,5 @@ import { z } from 'zod';
 
 export const signInSchema = z.object({
   identifier: z.string(),
-  password: z.string()
-})
+  password: z.string(),
+});
--- src/schemas/signUpSchema.ts
+++ src/schemas/signUpSchema.ts
@@ -4,10 +4,10 @@ export const usernameValidation = z
   .string()
   .min(3, 'Username must be at least 3 characters')
   .max(20, 'Username must be at most 20 characters')
-  .regex(/^[a-zA-Z0-9]+$/, 'Username must not contain special characters')
+  .regex(/^[a-zA-Z0-9]+$/, 'Username must not contain special characters');
 
 export const singUpSchema = z.object({
   username: usernameValidation,
   email: z.string().email({ message: 'Invalid Email Address' }),
-  password: z.string().min(8, { message: 'Password must be at least 8 characters' })
-})
+  password: z.string().min(8, { message: 'Password must be at least 8 characters' }),
+});
